7fe3d8eec51fdc090c9c95ea77d52415826c0806
Sam <sammeown@gmail.com>
2021-10-20 23:55:03 +0300

    Some refactoring

--- weather.py
+++ weather.py
@@ -1,22 +1,17 @@
-from typing import List
-
 import requests
 
 
-def get_weather(location: str):
+def get_weather(location: str) -> str:
     url = 'https://wttr.in/{location}'.format(location=location)
     queries = {'nTqm': '',
                'lang': 'ru'}
     response = requests.get(url, params=queries)
     response.raise_for_status()
 
-    print(response.text)
-
-
-def get_weathers(locations: List[str]):
-    for location in locations:
-        get_weather(location)
+    return response.text
 
 
 if __name__ == '__main__':
-    get_weathers(['Лондон', 'Шереметьево', 'Череповец'])
+    places = ['Лондон', 'Шереметьево', 'Череповец']
+    for place in places:
+        print(get_weather(place))
